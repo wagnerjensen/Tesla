d966d2e7d1f64b3f765886121475286be641b0d7
Wagnerjensen <wagnerjensen1962@gmail.com>
2024-08-15 17:13:28 -0300

    primeiro commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+let formulario = document.querySelector(".formulario")
+
+function aparecerformulario(){
+    formulario.style.left = "35%"
+}
+
+function sumirformulario() {
+formulario.style.left = "-50%"
+}
